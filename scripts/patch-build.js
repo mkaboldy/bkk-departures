6c68d8e1df1d6cdc6c7aa9f072e57121a23ba75e
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2023-02-12 10:24:06 +0100

    Local build for custom 404

--- scripts/patch-build.js
+++ scripts/patch-build.js
@@ -1,6 +1,6 @@
-import { copyFile } from "fs";
+const fs = require("fs");
 
-copyFile("docs/index.html", "docs/404.html", (err) => {
+fs.copyFile("docs/index.html", "docs/404.html", (err) => {
   if (err) {
     console.error("index.html -> 404.html copy failed:");
     throw err;
